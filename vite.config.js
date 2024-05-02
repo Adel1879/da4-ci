f8151affa2bad9343788a5f6179464b2d2d16198
Adel1879 <89564924+Adel1879@users.noreply.github.com>
2024-05-02 21:08:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/da4-ci/", // Add only this section
+  base: "", // Add only this section
 });
